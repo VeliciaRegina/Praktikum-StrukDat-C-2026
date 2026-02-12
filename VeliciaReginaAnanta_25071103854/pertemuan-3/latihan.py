74d084f0465d0f474068f8f50475dd3333aa9874
VeliciaRegina <veliciaregina27@gmail.com>
2026-02-12 21:19:47 +0700

    Uploadd latihan pertemuan 3

---
+++ VeliciaReginaAnanta_25071103854/pertemuan-3/latihan.py
@@ -0,0 +1,31 @@
+class Mahasiswa:
+   def __init__(self, nama, nim, kelas):
+      self.nama = nama
+      self.nim = nim
+      self.kelas = kelas
+
+   def memperkenalkan_diri (self, nama, nim):
+      print (f'Haloo nama saya {nama} dengan nim {nim}')
+
+   def update_nim (self, new_nim):
+      self.nim = new_nim
+
+mhs1 = Mahasiswa("Velicia", "25071100000", "TI C")
+mhs2 = Mahasiswa("Putri", '25071100001', 'TI B')
+mhs3 = Mahasiswa("Putra",'25071100002', "TI A")
+
+mhs1.update_nim("2502333333")
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self, name):
+    print('haloo, nama saya', name)
+
+orang1 = Person("cia", "18")
+
+orang1.greet()
+
+
